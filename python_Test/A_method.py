c332b890c430393ebc8f4cb9953f5788f594b2ec
Notingisimpossible <2421665649@qq.com>
2019-11-18 23:28:23 +0800

    A*method

--- python_Test/A_method.py
+++ python_Test/A_method.py
@@ -1,82 +1,83 @@
-import math
+#定义随机生成地图 实验五未用到此方法 故注释
+# def creatMap(x):
+#   print('hello')
+#   map = []
+#   print("Map is here:")
+#   for i in range(x):
+#     map.append([])
+#     for j in range(x):
+#       map[i].append(random.randint(0,1))
+#       if j==x-1:
+#         print(map[i][j])
+#       else:
+#         print(map[i][j],end="")
+#   map[0][0] = 0
+#   map [x-1][x-1] = 0
+#   return map
 
 
-the_map = [
-[0,0,0,0,0,0,0],
-[0,1,1,1,1,1,0],
-[0,1,0,1,0,0,0],
-[0,1,0,1,0,1,0],
-[0,0,0,1,0,1,0]]
-
-map_row = 5
-map_col= 7
-
+#生成节点
 class Node(object):
-    def __init__(self, _mine, _begin, _end):
+    #初始化节点
+    def __init__(self, now, begin, end):
         super(Node, self).__init__()
-
-        self.m_own = _mine
+        self.m_own = now
         self.m_g = 0
-        self.m_h = abs( _end[1] - _mine[1] ) + abs( _end[0] - _mine[0] )
+        self.m_h = abs( end[1] - now[1] ) + abs( end[0] - now[0] )#计算H值
         self.m_f = self.m_g + self.m_h
-        self.m_begin = _begin
-        self.m_end = _end
-        self.m_tag = str( _mine )
-        self.m_neighbors = []
+        self.m_begin = begin
+        self.m_end = end
+        self.m_flag = str( now )
+        self.m_neighbors = []#用来存储当点节点的邻居节点
         self.m_parent = None
-
-    def set_gvalue(self, g):
-        self.m_g = g
-    def get_gvalue(self):
-        return self.m_g
-    gvalue = property( get_gvalue, set_gvalue )
-
-    def set_parent(self, parent):
-        self.m_parent = parent
-    def get_parent(self):
-        return self.m_parent
-    parent = property( get_parent, set_parent )
-
-    def get_tag(self):
-        return self.m_tag
-
+    
+    #返回当前节点的字符串形式
+    def get_flag(self):
+        return self.m_flag
+    #得到F值
     def get_fvalue(self):
         return self.m_f
-
+    # 得到当前节点
     def get_position(self):
         return self.m_own
-
+    #把邻居节点设为当前节点的父节点
+    def set_parent(self, parentN):
+        self.m_parent = parentN
+    #得到父节点
+    def get_parent(self):
+        return self.m_parent
+    #调用方法得到父节点
+    parentN = property( get_parent, set_parent )
+     # 设置G值
+    def set_gvalue(self, g):
+        self.m_g = g
+    #得到G值
+    def get_gvalue(self):
+        return self.m_g
+    #调用上面方法设置并得G值
+    gvalue = property( get_gvalue, set_gvalue )
+    #得到当前格子的相邻的格子
     def get_neighbors(self):
-        nei = [[0,1],[0,-1],[1,0],[-1,0]]
-        for item in nei:
+        #nei为上下左右四个方向，数组值为当前节点移动距离，例如[0,1]说明当前节点向右移动一格
+        neb = [[0,1],[0,-1],[1,0],[-1,0]]
+        #循环遍历当前节点四周节点
+        for item in neb:
             x = item[0] + self.m_own[0]
             y = item[1] + self.m_own[1]
-            if (x >= 0 and x < map_row) and (y >= 0 and y < map_col):
-                if the_map[x][y] == 0:
+            if (x >= 0 and x < map_size) and (y >= 0 and y < map_size):
+                if map[x][y] == 0:
                     self.m_neighbors.append( Node( (x, y), self.m_begin, self.m_end ) )
 
         return self.m_neighbors
 
-def findMinNode( _list ):
-    _min = _list[0]
-    for x in _list:
-        if x.get_fvalue() < _min.get_fvalue():
-            _min = x
-
-    return _min
-
-def isContains( _list, _node ):
-    for item in _list:
-        if item.get_tag() == _node.get_tag():
-            return True
-    return False
-
-def aStarSearch( _begin, _end ):
+#寻路函数
+def aStarSearch( begin, end ):
+    # 定义 未判断数组openList 已判断数组colseList
     openlist = []
     closelist = []
-    #把起点加入open list
 
-    begin_node = Node( _begin, _begin, _end )
+    #把起点加入open list
+    begin_node = Node( begin, begin, end )
     begin_node.gvalue = 0
     openlist.append( begin_node )
 
@@ -93,24 +94,52 @@ def aStarSearch( _begin, _end ):
         neighbors = _min.get_neighbors()
         for item in neighbors:
             if not isContains( openlist, item ) and not isContains( closelist, item ):
-                item.parent = _min
+                item.parentN = _min
                 item.gvalue = _min.gvalue + 1
                 openlist.append( item )
 
 
         #如果终点在openlist中，直接返回终点格子
         for x in openlist:
-            if x.get_tag() == str(_end):
+            if x.get_flag() == str(end):
                 return x
 
     #openlist用尽，仍然找不到终点，说明终点不可到达，返回空
     return None
 
+#找到并返回openList中起点距离终点最近的点并返回
+def findMinNode( _list ):
+    _min = _list[0]
+    for x in _list:
+        if x.get_fvalue() < _min.get_fvalue():
+            _min = x
 
-dst = aStarSearch( (2,2),(3,4) )
-path = [dst.get_tag()]
-while dst.parent:
-    dst = dst.parent
-    path.insert( 0, dst.get_tag() )
+    return _min
+#判断节点是否在openList或closeList中
+def isContains( _list, _node ):
+    for item in _list:
+        if item.get_flag() == _node.get_flag():
+            return True
+    return False
 
-print( path )
+
+# 定义地图
+map = [
+[0,0,0,0,0],
+[1,0,1,0,1],
+[0,1,1,1,1],
+[0,1,0,0,0],
+[0,0,0,1,0]]
+
+map_size = 5
+dst= aStarSearch((0,0),(4,4))
+# 异常处理 如果迷宫无解则dst最后一个节点为None,它将不会有get_flag方法，发生异常，此时对异常进行处理
+try:
+    path = [dst.get_flag()]
+    while dst.parentN:
+        dst = dst.parentN
+        path.insert( 0, dst.get_flag() )
+    print( path )
+except AttributeError:
+    print('该迷宫无解！')
+    pass
