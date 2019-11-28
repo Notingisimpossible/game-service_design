a5d00f7235d9656eb6d0a7331c7add946bde06f3
Notingisimpossible <2421665649@qq.com>
2019-11-28 18:08:25 +0800

    leetcode

---
+++ leetcode/devide.py
@@ -0,0 +1,18 @@
+# class Solution(object):
+#     def divide(self, dividend, divisor):
+#         """
+#         :type dividend: int
+#         :type divisor: int
+#         :rtype: int
+#         """
+
+# x = int(16)
+# h = int(10)
+# y = bin(x)
+# z = oct(h)
+# k = int(y,2)
+# print(y,z,k)
+
+x = 10
+y = x>>2
+print(y)
